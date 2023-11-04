3e2b49f4f79c50c1ccd59db1ea7ac204de3c2333
Rubén Chiquin <randreu28@gmail.com>
2023-11-04 19:20:09 +0100

    Makes use of kysely camelCase plugin

--- backend/utils/kysely/index.ts
+++ backend/utils/kysely/index.ts
@@ -1,9 +1,10 @@
-import { Kysely } from "kysely";
+import { CamelCasePlugin, Kysely } from "kysely";
 import { dialect } from "./dialect";
 import { DB } from "./tables.g";
 
 export const db = new Kysely<DB>({
   dialect,
+  plugins: [new CamelCasePlugin()],
   log: (log) => {
     console.log(log.query.sql);
   },

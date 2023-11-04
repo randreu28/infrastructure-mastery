91e8112e4461881b5fb47ebcb7564d5dba1c9225
Rubén Chiquin <randreu28@gmail.com>
2023-11-04 20:04:33 +0100

    Creates a population script

--- backend/utils/populate/populate.ts
+++ backend/utils/populate/populate.ts
@@ -11,12 +11,46 @@ async function generateRandomData(amount: number, seed: number) {
   for (let i = 0; i < amount; i++) {
     console.log("Fake data #", i);
 
-    /* ... */
+    const user = await db
+      .insertInto("users")
+      .values({
+        name: faker.person.fullName(),
+        oauthId: faker.string.uuid(),
+      })
+      .returningAll()
+      .executeTakeFirstOrThrow();
+
+    const postAmount = Math.floor(Math.random() * 4) + 2;
+    for (let j = 0; j < postAmount; j++) {
+      const post = await db
+        .insertInto("posts")
+        .values({
+          createdAt: faker.date.recent(),
+          content: faker.lorem.paragraphs(),
+          title: faker.lorem.sentence(),
+          userId: Number(user.id),
+        })
+        .returningAll()
+        .executeTakeFirstOrThrow();
+
+      const commentAmount = Math.floor(Math.random() * 3) + 2;
+      for (let k = 0; k < commentAmount; k++) {
+        await db
+          .insertInto("comments")
+          .values({
+            content: faker.lorem.sentences(2),
+            createdAt: faker.date.recent(),
+            postId: Number(post.id),
+            userId: Number(user.id),
+          })
+          .executeTakeFirstOrThrow();
+      }
+    }
   }
   process.exit();
 }
 
 const numEntries = process.argv[2] ? parseInt(process.argv[2]) : 30;
-const randomSeed = process.argv[3] ? parseInt(process.argv[3]) : 123;
+const randomSeed = process.argv[3] ? parseInt(process.argv[3]) : 100;
 
 generateRandomData(numEntries, randomSeed);

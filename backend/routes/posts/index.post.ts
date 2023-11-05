958adf89d11842f7bdd2fe9c9f8771bbcad33078
Rubén Chiquin <randreu28@gmail.com>
2023-11-05 11:36:49 +0100

    Creates PUT & GET endpoint for posts

---
+++ backend/routes/posts/index.post.ts
@@ -0,0 +1,18 @@
+import { postSchema } from "../../utils/schemas";
+
+export default eventHandler(async (event) => {
+  const post = await readValidatedBody(event, postSchema.safeParse);
+
+  if (post.success === false) {
+    return sendErr(event, {
+      statusCode: 400,
+      data: post.error,
+    });
+  }
+
+  return db
+    .insertInto("posts")
+    .values({ ...post.data, createdAt: new Date() })
+    .returningAll()
+    .executeTakeFirst();
+});
--- backend/utils/schemas.ts
+++ backend/utils/schemas.ts
@@ -1,14 +1,12 @@
 import { z } from "zod";
 
 export const postSchema = z.object({
-  createdAt: z.date(),
   title: z.string().max(200),
   author: z.string().max(200),
   content: z.string(),
 });
 
 export const commentScema = z.object({
-  createdAt: z.date(),
   postId: z.number(),
   content: z.string(),
 });

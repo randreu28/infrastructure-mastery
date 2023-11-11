cb9cd237ac1a29d6164102a270f2869340eb2d17
Rubén Chiquin <randreu28@gmail.com>
2023-11-11 20:11:09 +0100

    Fixes /[id] routes to return specific resource

--- backend/routes/comments/[id].get.ts
+++ backend/routes/comments/[id].get.ts
@@ -1,3 +1,25 @@
-export default eventHandler(() => {
-  return db.selectFrom("comments").selectAll().execute();
+export default eventHandler((event) => {
+  const id = Number(event.context.params?.id);
+
+  if (isNaN(id)) {
+    return sendErr(event, {
+      statusCode: 400,
+      statusMessage: "Parameter is not a number",
+    });
+  }
+
+  const comment = db
+    .selectFrom("comments")
+    .where("id", "=", id)
+    .selectAll()
+    .executeTakeFirst();
+
+  if (!comment) {
+    return sendErr(event, {
+      statusCode: 404,
+      statusMessage: `Comment notfound with id ${id}`,
+    });
+  }
+
+  return comment;
 });
--- backend/routes/posts/[id].get.ts
+++ backend/routes/posts/[id].get.ts
@@ -1,3 +1,25 @@
-export default eventHandler(() => {
-  return db.selectFrom("posts").selectAll().execute();
+export default eventHandler(async (event) => {
+  const id = Number(event.context.params?.id);
+
+  if (isNaN(id)) {
+    return sendErr(event, {
+      statusCode: 400,
+      statusMessage: "Parameter is not a number",
+    });
+  }
+
+  const post = await db
+    .selectFrom("posts")
+    .where("id", "=", id)
+    .selectAll()
+    .executeTakeFirst();
+
+  if (!post) {
+    return sendErr(event, {
+      statusCode: 404,
+      statusMessage: `Post notfound with id ${id}`,
+    });
+  }
+
+  return post;
 });

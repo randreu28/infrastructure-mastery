49288dad1f2d2c11a683b9b74539f70a6e09845e
Rubén Chiquin <randreu28@gmail.com>
2023-11-11 21:13:12 +0100

    Creates a Jotai store

--- frontend/src/lib/SelectedPost.ts
+++
@@ -1,4 +0,0 @@
-import { atom } from "jotai";
-
-/* TODO: Figure how I want to handle state */
-export const selectedPost = atom(10);
---
+++ frontend/src/lib/store.ts
@@ -0,0 +1,3 @@
+import { atom } from "jotai";
+
+export const postStore = atom<number | undefined>(undefined);
